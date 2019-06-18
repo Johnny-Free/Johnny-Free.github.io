a34611362c2be0bc1b17cf81d983d6e2cc93d788
Johnny Free <j.darell.free@gmail.com>
2019-06-18 15:31:14 -0500

    changing path to eor page

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
 var path = require('path');
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+const lin = process.env.eUser;
+const uid = process.env.eLogin;
 
 app.use(express.static('resources'));
 
@@ -20,4 +33,39 @@ app.get('/projects', function(req,res){
     res.sendFile(path.join(__dirname + '/projects.html'))
 });
 
-app.listen(8080);
+app.get('/contact', function(req,res){
+    res.sendFile(path.join(__dirname + '/contact.html'))
+});
+
+app.post('/contact', (req,res) => {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user:`${lin}`,
+            pass:`${uid}`
+        }
+    });
+
+    const mailOptions = {
+        from: `${lin}`,
+        to: `${lin}`,
+        // cc: `$req.body.rEmail`,
+        subject: `${req.body.rEmail}`,
+        html:
+        `Requestor: ${req.body.rName}
+        <br/>
+        Request: ${req.body.eBody}`
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+        if(err)
+            console.log(err)
+        else {
+            console.log('email log', info)
+        }
+    });
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
